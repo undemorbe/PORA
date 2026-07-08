b7c856a5803d4cce0b8c3a0688d1c9d64b47f72a
boriS <dolbobob00@gmail.com>
2026-07-08 15:56:26 +0300

    chore: deeplinks integration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.5.0" apply false
+}
+
+dependencies{
+    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
 }
 
 val keystoreProperties = Properties().apply {
@@ -26,6 +31,7 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
@@ -60,3 +66,5 @@ android {
 flutter {
     source = "../.."
 }
+
+apply plugin: 'com.google.gms.google-services'
